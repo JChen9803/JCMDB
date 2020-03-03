72ea01b8633a79b32f49161f69ef6d19315f8b34
Jason Chen <jc4rn@virginia.edu>
2020-03-02 22:14:02 -0500

    Home and user page added

---
+++ js/main.js
@@ -0,0 +1,97 @@
+function movieValidation() {
+    var movieTitle = document.getElementById("inputMovieTitle").value;
+    var year = document.getElementById("inputYear").value;
+    var genre = document.getElementById("inputGenre").value;
+    var stars = document.getElementById("inputActors").value;
+    var directors = document.getElementById("inputDirectors").value;
+    var summary = document.getElementById("inputSummary").value;
+
+    let valid = true;
+
+    if(movieTitle == "") {
+        valid = false;
+        document.getElementById("inputMovieTitle").style.borderColor = "red";
+        document.getElementById("movieTitleError").style.visibility = "visible";
+    }
+    else {
+        document.getElementById("inputMovieTitle").style.borderColor = "#32CD32";
+        document.getElementById("movieTitleError").style.visibility = "hidden";
+    }
+
+    let pat = /[0-9]{4}/;
+    if(year == "" || !pat.test(year)) {
+        valid = false;
+        document.getElementById("inputYear").style.borderColor = "red";
+        document.getElementById("yearError").style.visibility = "visible";
+    }
+    else {
+        document.getElementById("inputYear").style.borderColor = "#32CD32";
+        document.getElementById("yearError").style.visibility = "hidden";
+    }
+
+    if(genre == "Choose...") {
+        valid = false;
+        document.getElementById("inputGenre").style.borderColor = "red";
+        document.getElementById("genreError").style.visibility = "visible";
+    }
+    else {
+        document.getElementById("inputGenre").style.borderColor = "#32CD32";
+        document.getElementById("genreError").style.visibility = "hidden";
+    }
+
+    if(stars == "") {
+        valid = false;
+        document.getElementById("inputActors").style.borderColor = "red";
+        document.getElementById("actorError").style.visibility = "visible";
+    }
+    else {
+        document.getElementById("inputActors").style.borderColor = "#32CD32";
+        document.getElementById("actorError").style.visibility = "hidden";
+    }
+
+    if(directors == "") {
+        valid = false;
+        document.getElementById("inputDirectors").style.borderColor = "red";
+        document.getElementById("directorError").style.visibility = "visible";
+    }
+    else {
+        document.getElementById("inputDirectors").style.borderColor = "#32CD32";
+        document.getElementById("directorError").style.visibility = "hidden";
+    }
+
+    if(summary == "") {
+        valid = false;
+        document.getElementById("inputSummary").style.borderColor = "red";
+        document.getElementById("summaryError").style.visibility = "visible";
+    }
+    else {
+        document.getElementById("inputSummary").style.borderColor = "#32CD32";
+        document.getElementById("summaryError").style.visibility = "hidden";
+    }
+
+    if (valid) {
+
+    }
+
+}
+
+let resetMovieModal = () => {
+    document.getElementById("inputMovieTitle").style.removeProperty("border-color");
+    document.getElementById("inputYear").style.removeProperty("border-color");
+    document.getElementById("inputGenre").style.removeProperty("border-color");
+    document.getElementById("inputActors").style.removeProperty("border-color");
+    document.getElementById("inputDirectors").style.removeProperty("border-color");
+    document.getElementById("inputSummary").style.removeProperty("border-color");
+    document.getElementById("movieTitleError").style.visibility = "hidden";
+    document.getElementById("yearError").style.visibility = "hidden";
+    document.getElementById("genreError").style.visibility = "hidden";
+    document.getElementById("actorError").style.visibility = "hidden";
+    document.getElementById("directorError").style.visibility = "hidden";
+    document.getElementById("summaryError").style.visibility = "hidden";
+    document.getElementById("inputMovieTitle").value = "";
+    document.getElementById("inputYear").value = "";
+    document.getElementById("inputGenre").value = "Choose...";
+    document.getElementById("inputActors").value = "";
+    document.getElementById("inputDirectors").value = "";
+    document.getElementById("inputSummary").value = "";
+}
---
+++ js/userInfo.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+    document.getElementById("editProfile").onclick = function() {
+        document.getElementById("inputUsername").value = document.getElementById("name").innerText;
+        document.getElementById("inputAge").value = document.getElementById("age").innerText;
+        document.getElementById("inputFavMovie").value = document.getElementById("favMovie").innerText;
+        document.getElementById("inputFavStar").value = document.getElementById("favStar").innerText;
+        document.getElementById("inputBlurb").value = document.getElementById("userBlurb").innerText;
+    };
+}
+
+function updateUserInfo() {
+    document.getElementById("name").innerText = document.getElementById("inputUsername").value;
+    document.getElementById("age").innerText = document.getElementById("inputAge").value;
+    document.getElementById("favMovie").innerText = document.getElementById("inputFavMovie").value;
+    document.getElementById("favStar").innerText = document.getElementById("inputFavStar").value;
+    document.getElementById("userBlurb").innerText = document.getElementById("inputBlurb").value;
+    document.getElementById("closeUserModal").click();
+    document.getElementById("updateProfileSuccessMessage").style.display = "block";
+
+    $("#closeUserModal").click(function(){
+        $("#updateProfileSuccessMessage").fadeOut();
+    });
+}
